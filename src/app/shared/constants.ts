90c6f999b3f680f414495f73bd2172d0c29a7244
Konstantin Merkushev <tranzistance@gmail.com>
2019-03-06 10:31:08 +0300

    подсветка блока с кодом

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,23 +1,34 @@
-export const languages = [
+interface LanguageMode {
+  name: string;
+  version?: number;
+}
+
+export interface Language {
+  id: number;
+  title: string;
+  mode: string | LanguageMode;
+}
+
+export const languages: Language[] = [
   {
     id: 1,
     title: 'Free Pascal 3.0.2',
-    mode: 'pascal',
+    mode: 'text/x-pascal',
   },
   {
     id: 2,
     title: 'GNU C 7.2.0',
-    mode: 'clike',
+    mode: 'text/x-csrc',
   },
   {
     id: 3,
     title: 'GNU C++ 7.2.0',
-    mode: 'clike',
+    mode: 'text/x-c++src',
   },
   {
     id: 7,
     title: 'Turbo Pascal',
-    mode: 'pascal',
+    mode: 'text/x-pascal',
   },
   {
     id: 8,
@@ -27,52 +38,58 @@ export const languages = [
   {
     id: 9,
     title: 'Borland C',
-    mode: 'clike',
+    mode: 'text/x-csrc',
   },
   {
     id: 10,
     title: 'Borland C++',
-    mode: 'clike',
+    mode: 'text/x-c++src',
   },
   {
     id: 18,
     title: 'Java JDK 1.8',
-    mode: 'clike',
+    mode: 'text/x-java',
   },
   {
     id: 22,
     title: 'PHP PHP 7.1.13',
-    mode: 'php',
+    mode: 'application/x-httpd-php',
   },
   {
     id: 23,
     title: 'Python 2.7.10',
-    mode: 'python',
+    mode: {
+      name: 'text/x-python',
+      version: 2,
+    },
   },
   {
     id: 24,
     title: 'Perl 5.26.1',
-    mode: 'perl',
+    mode: 'text/x-perl',
   },
   {
     id: 25,
     title: 'Mono C# 4.8',
-    mode: 'clike',
+    mode: 'text/x-csharp',
   },
   {
     id: 26,
     title: 'Ruby 2.4.3',
-    mode: 'ruby',
+    mode: 'text/x-ruby',
   },
   {
     id: 27,
     title: 'Python 3.6.4',
-    mode: 'python',
+    mode: {
+      name: 'text/x-python',
+      version: 3,
+    },
   },
   {
     id: 28,
     title: 'Haskell GHC 8.0.2',
-    mode: 'haskell',
+    mode: 'text/x-haskell',
   },
   {
     id: 29,
@@ -80,12 +97,13 @@ export const languages = [
     mode: 'pascal',
   },
   {
+    id: 30,
     title: 'PascalABC 3.1.0.1198',
-    mode: 'pascal',
+    mode: 'text/x-pascal',
   },
   {
     id: 68,
     title: 'GNU C++ 5.3.1 + sanitizer',
-    mode: 'clike',
+    mode: 'text/x-c++src',
   },
 ];
