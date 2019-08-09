3d387a9eb17b9267139b2d5c9b1ea827b1c6dcd0
Tokarev Ivan <i.tokarev@tinkoff.ru>
2019-08-09 14:40:57 +0300

    Status time in monitor table fixed.

--- src/app/pages/monitor/monitor-container/minute-seconds.pipe.ts
+++ src/app/pages/monitor/monitor-container/minute-seconds.pipe.ts
@@ -1,13 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'minuteSeconds'
+  name: 'minuteSeconds',
 })
 export class MinuteSecondsPipe implements PipeTransform {
 
   transform(initialValue: number): string {
     const negative = initialValue < 0;
-    const value = Math.floor(Math.abs(initialValue / 1000));
+    const value = Math.abs(initialValue);
     const minutes: number = Math.floor(value / 60);
     return `${negative ? '-' : ''}${minutes.toString().padStart(2, '0')}:${(value - minutes * 60).toString().padStart(2, '0')}`;
   }
