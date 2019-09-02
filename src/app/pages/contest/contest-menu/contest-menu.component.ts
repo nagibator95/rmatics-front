3dc11d36d025e65c929e08b8179a6a2191571da9
Tokarev Ivan <i.tokarev@tinkoff.ru>
2019-09-02 11:14:21 +0300

    Meeting text format fixed.

--- src/app/pages/contest/contest-menu/contest-menu.component.ts
+++ src/app/pages/contest/contest-menu/contest-menu.component.ts
@@ -2,8 +2,9 @@ import {
   ChangeDetectionStrategy,
   Component,
   HostBinding,
-  Input,
+  Input, OnInit,
 } from '@angular/core';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
 import {Store} from '@ngrx/store';
 
 import {RouterActions} from '../../../core/stores/router';
@@ -23,7 +24,7 @@ interface Task {
   styleUrls: ['./contest-menu.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ContestMenuComponent {
+export class ContestMenuComponent implements OnInit {
   @HostBinding('class._collapsed') @Input() collapsed = false;
   @Input() meetingText!: string;
   @Input() meetingLink!: string;
@@ -31,9 +32,16 @@ export class ContestMenuComponent {
   @Input() timer!: string;
   @Input() tasks!: Task[];
   @Input() currentTaskId = 1;
+  safeHtml: SafeHtml;
   workshop = this.workshopService.workshop;
 
-  constructor(private workshopService: WorkshopService, private store$: Store<any>) {}
+  constructor(private workshopService: WorkshopService, private store$: Store<any>, private sanitizer: DomSanitizer) {}
+
+  ngOnInit() {
+    this.safeHtml = this.sanitizer.bypassSecurityTrustHtml(
+      this.meetingText,
+    );
+  }
 
   toggleMenu = () => this.collapsed = !this.collapsed;
 
